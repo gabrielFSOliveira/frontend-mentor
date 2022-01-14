ebdf489f2890b0610baba5852dd06ee4aa434c54
Gabriel Felipe <88507281+gabrielFSOliveira@users.noreply.github.com>
2022-01-14 00:40:58 -0300

    separating objects and js

--- js/script.js
+++ js/script.js
@@ -1,44 +1,10 @@
-const cards = [
-    {
-        img: './imagens/desafios-imagens/3-column-preview-card-component-main.png',
-        challenge: '3 Column Preview Card Component Main',
-        page_link: 'https://gabrielfsoliveira.github.io/frontend-mentor/desafios/3-column-preview-card-component-main/',
-        code_link: 'https://github.com/gabrielFSOliveira/frontend-mentor/tree/main/desafios/3-column-preview-card-component-main',
-        level: 'newbie'
-    },
-    {
-        img: 'imagens/desafios-imagens/single-price-grid-component-master.png',
-        challenge: 'Single Price Grid Component Master',
-        page_link: 'https://gabrielfsoliveira.github.io/frontend-mentor/desafios/single-price-grid-component-master/',
-        code_link: 'https://github.com/gabrielFSOliveira/frontend-mentor/tree/main/desafios/single-price-grid-component-master',
-        level: 'newbie'
-    },
-    {
-        img: './imagens/desafios-imagens/order-summary-component-main.png',
-        challenge: 'Order Summary Component',
-        page_link: 'https://gabrielfsoliveira.github.io/frontend-mentor/desafios/order-summary-component-main/',
-        code_link: 'https://github.com/gabrielFSOliveira/frontend-mentor/tree/main/desafios/order-summary-component-main/',
-        level: 'newbie'
-    },
-    {
-        img: './imagens/desafios-imagens/testimonials-grid-section-main.png',
-        challenge: 'Testimonials Grid Section',
-        page_link: 'https://gabrielfsoliveira.github.io/frontend-mentor/desafios/testimonials-grid-section-main/',
-        code_link: 'https://github.com/gabrielFSOliveira/frontend-mentor/tree/main/desafios/testimonials-grid-section-main',
-        level: 'junior'
-    },
-    {
-        img: './imagens/desafios-imagens/nft-preview-card-component.png',
-        challenge: 'NFT Preview Card Component',
-        page_link: 'https://gabrielfsoliveira.github.io/frontend-mentor/desafios/nft-preview-card-component-main/',
-        code_link: 'https://github.com/gabrielFSOliveira/frontend-mentor/tree/main/desafios/nft-preview-card-component-main',
-        level: 'newbie'
-    },
-]
+import challenges from './challenges.json' assert { type: "json" }
 
-cards.map(card =>{
-    const {img, challenge, page_link, code_link, level} = card
-    
+const cards = challenges
+
+cards.map(card => {
+    const { img, challenge, page_link, code_link, level } = card
+
     const div_card = document.createElement('div')
     div_card.classList.add('card')
 
@@ -56,6 +22,6 @@ cards.map(card =>{
 
     if (level == 'newbie')
         document.querySelector('.newbie-div').appendChild(div_card)
-    else if(level == 'junior')
+    else if (level == 'junior')
         document.querySelector('.junior-div').appendChild(div_card)
 })
